ab3821ef71eb861177340156705450184ddb2b09
Shiela-P <125091812+Shiela-P@users.noreply.github.com>
2026-03-27 13:44:44 -0400

    Learning how to hanke click events and pass arguments

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,18 @@
 // shortcut sfc (stateless functional component)
 const Home = () => {
+    const handleClick = () => {
+        console.log('Wow... You just had to click it...')
+    }
+
+    const handleCLick2 = (name) =>{
+        console.log("Good job, " + name + "!")
+    }
     return ( 
         <div className="home">
             <h2>Home Page</h2>
+            <button onClick={handleClick}>Do not click</button>
+            {/* How to pass an argument without immediately activating the function on page load */}
+            <button onCLick={() => {handleCLick2("you")}} >Click here!</button>
         </div>
      );
 }
