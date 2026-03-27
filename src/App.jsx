4964ba2e2270916782422d27abc84123eb0f940f
Shiela-P <125091812+Shiela-P@users.noreply.github.com>
2026-03-27 13:26:47 -0400

    Messing around with CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import './App.css'
+import Navbar from './Navbar.jsx'
+import Home from './Home'
 
 function App() {
-  const title = "Hello there! Welcome to my React App.";
-  const coolThing = "cupckake";
+  // const title = "Hello there! Welcome to my React App.";
+  // const coolThing = "cupckake";
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
+        <Home />
         {/* Woah! this is a variable */}
-        <h1>{title}</h1>
-        <p>Today I'm really fascinated by {coolThing}s!</p>
-        <p>I can put any javaScript between curly braces if it equates to a value! For example {3+8}.</p>
+        {/* <h1>{title}</h1> */}
+
+       
       </div>
     </div>
   )
---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+// shortcut sfc (stateless functional component)
+const Home = () => {
+    return ( 
+        <div className="home">
+            <h2>Home Page</h2>
+        </div>
+     );
+}
+
+
+ 
+export default Home;
