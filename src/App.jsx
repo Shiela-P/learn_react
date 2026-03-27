63d42257e9ec91cc53fa7328a39d1d8a91e1180c
Shiela-P <125091812+Shiela-P@users.noreply.github.com>
2026-03-26 21:21:36 -0400

    Learing about variables and dynamic content with JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
 
 function App() {
+  const title = "Hello there! Welcome to my React App.";
+  const coolThing = "cupckake";
   return (
     <div className="App">
       <div className="content">
-        <h1>Welcome to My React App</h1>
+        {/* Woah! this is a variable */}
+        <h1>{title}</h1>
+        <p>Today I'm really fascinated by {coolThing}s!</p>
+        <p>I can put any javaScript between curly braces if it equates to a value! For example {3+8}.</p>
       </div>
     </div>
   )
