3b5246fcc2e788c65f5ad044f81c4fce55034ef1
Shiela-P <125091812+Shiela-P@users.noreply.github.com>
2026-03-26 21:10:29 -0400

    starting out with the simplest steps

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="content">
+        <h1>Welcome to My React App</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App;
