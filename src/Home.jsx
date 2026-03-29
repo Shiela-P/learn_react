8ff0515e7c0ec82fe1a4406f04520f17a83ebd3a
Shiela-P <125091812+Shiela-P@users.noreply.github.com>
2026-03-29 18:48:18 -0400

    Learning about hooks

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,18 +1,18 @@
+import {useState} from 'react';
+
 // shortcut sfc (stateless functional component)
 const Home = () => {
+    const [food, setFood] = useState('cupcakes');
     const handleClick = () => {
-        console.log('Wow... You just had to click it...')
+       setFood('Chicken Wing');
     }
 
-    const handleCLick2 = (name) =>{
-        console.log("Good job, " + name + "!")
-    }
     return ( 
         <div className="home">
             <h2>Home Page</h2>
             <button onClick={handleClick}>Do not click</button>
-            {/* How to pass an argument without immediately activating the function on page load */}
-            <button onCLick={() => {handleCLick2("you")}} >Click here!</button>
+            <p>{food}</p>
+            
         </div>
      );
 }
