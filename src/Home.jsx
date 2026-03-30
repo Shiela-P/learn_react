0a92326f13ad6726dafddd4811732b9c8ae44558
Shiela-P <125091812+Shiela-P@users.noreply.github.com>
2026-03-29 21:10:16 -0400

    Outputting lists and using the map function

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,17 +2,19 @@ import {useState} from 'react';
 
 // shortcut sfc (stateless functional component)
 const Home = () => {
-    const [food, setFood] = useState('cupcakes');
-    const handleClick = () => {
-       setFood('Chicken Wing');
-    }
-
+    const [blogs, setBlogs] = useState([
+        {title: "Welcome", body: "lorem ipsum...", author: "me", id:1},
+        {title: "A day in the life of an Oreo Cookie Tester", body: "Lorem ipsum...", author: "Myself", id:"2"},
+        {title: "I make 6 figures just playing with puppies all day", body: "Loerm ipsum...", author: "I", id:"3"}
+    ]);
     return ( 
         <div className="home">
-            <h2>Home Page</h2>
-            <button onClick={handleClick}>Do not click</button>
-            <p>{food}</p>
-            
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
         </div>
      );
 }
