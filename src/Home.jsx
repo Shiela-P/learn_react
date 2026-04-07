75a28a4a29d49f0aee6cdf4c5f5303a4bb230a42
Shiela-P <125091812+Shiela-P@users.noreply.github.com>
2026-04-07 17:02:45 -0400

    JSON server

--- src/Home.jsx
+++ src/Home.jsx
@@ -8,6 +8,8 @@ const Home = () => {
         {title: "I make 6 figures just playing with puppies all day", body: "Loerm ipsum...", author: "I", id:"3"}
     ]);
 
+    const [name, setName] = useState('pikachu');
+
     const handleDelete = (id) => {
         // Creates a new array of blogs using evrything except the excluded id
         const newBlogs = blogs.filter(blog => blog.id !== id)
@@ -15,12 +17,14 @@ const Home = () => {
     }
 
     useEffect(() => {
-        // alert("test");
-    })
+        alert("test");
+    }, [name])
 
     return ( 
         <div className="home">
            <BlogList blogs={blogs} title="My Blogs" handleDelete={handleDelete}/>
+           <button onClick={() => setName("charmander")}>Change Name</button>
+           <p>{ name }</p>
         </div>
      );
 }
