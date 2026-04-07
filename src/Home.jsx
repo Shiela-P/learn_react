e0f9b2a3277d7c0b5e9a69a7a4032cac5fdb0491
Shiela-P <125091812+Shiela-P@users.noreply.github.com>
2026-04-07 17:56:13 -0400

    Fetching and displaying data

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,13 +2,8 @@ import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
 // shortcut sfc (stateless functional component)
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {title: "Welcome", body: "lorem ipsum...", author: "me", id:1},
-        {title: "A day in the life of an Oreo Cookie Tester", body: "Lorem ipsum...", author: "Myself", id:"2"},
-        {title: "I make 6 figures just playing with puppies all day", body: "Loerm ipsum...", author: "I", id:"3"}
-    ]);
+    const [blogs, setBlogs] = useState(null);
 
-    const [name, setName] = useState('pikachu');
 
     const handleDelete = (id) => {
         // Creates a new array of blogs using evrything except the excluded id
@@ -17,14 +12,19 @@ const Home = () => {
     }
 
     useEffect(() => {
-        alert("test");
-    }, [name])
+        fetch('http://localhost:8000/blogs')
+        .then(res => {
+           return res.json()
+        }) 
+        .then((data) => {
+            setBlogs(data);
+        })
+    }, [])
 
     return ( 
         <div className="home">
-           <BlogList blogs={blogs} title="My Blogs" handleDelete={handleDelete}/>
-           <button onClick={() => setName("charmander")}>Change Name</button>
-           <p>{ name }</p>
+          {blogs && <BlogList blogs={blogs} title="My Blogs" handleDelete={handleDelete}/>}
+
         </div>
      );
 }
