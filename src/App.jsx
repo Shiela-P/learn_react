9532cfec1a311486bdb1ebd1346588edcd833c76
Shiela-P <125091812+Shiela-P@users.noreply.github.com>
2026-04-14 19:46:44 -0400

    learning about React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
 import Navbar from './Navbar.jsx'
 import Home from './Home'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
   // const title = "Hello there! Welcome to my React App.";
   // const coolThing = "cupckake";
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
-        {/* Woah! this is a variable */}
-        {/* <h1>{title}</h1> */}
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Home />
+          {/* Woah! this is a variable */}
+          {/* <h1>{title}</h1> */}
 
-       
+        
+        </div>
       </div>
-    </div>
-  )
+    </Router>
+  );
 }
 
 export default App;
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,29 +1,10 @@
 import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 // shortcut sfc (stateless functional component)
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
+   const {data: blogs, isPending, error} = useFetch('http://localhost:3000/blogs');
 
-
-    useEffect(() => {
-        fetch('http://localhost:3000/blogs')
-            .then(res => {
-                if(!res.ok){
-                    throw Error("could not fetch data");
-                }
-            return res.json()
-            }) 
-            .then((data) => {
-                setBlogs(data);
-                setIsPending(false);
-            })
-            .catch(err => {
-                setIsPending(false);
-                setError(err.message);
-            });
-        }, [])
 
     return ( 
         <div className="home">
