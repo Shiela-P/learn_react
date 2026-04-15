89a873451d7330f070af2969e38291f4bdd2e7e4
Shiela-P <125091812+Shiela-P@users.noreply.github.com>
2026-04-14 20:19:31 -0400

    Learning more about making multiple pages in React usimg React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
 import Navbar from './Navbar.jsx'
 import Home from './Home'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Create from './Create.jsx';
 
 function App() {
   // const title = "Hello there! Welcome to my React App.";
   // const coolThing = "cupckake";
   return (
+    // Everything is warpped in the Router component
     <Router>
       <div className="App">
+        {/* We want navbar on every page so it's not wrapped in switch */}
         <Navbar />
         <div className="content">
-          <Home />
-          {/* Woah! this is a variable */}
-          {/* <h1>{title}</h1> */}
+          {/* switch component allows me to have multiple "pages" with only one showing at a time */}
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/create">
+              <Create />
+            </Route>
+          </Switch>
 
         
         </div>
